c47ba39d53e25559fdb00c049271f6954bb52851
mashrur ayon <40263721+mashrur-ayon@users.noreply.github.com>
2020-06-21 19:57:46 +0600

    makeCacheMatrix function.
    The R script was edited to complete the makeCacheMatrix function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Script for the completion of Week 3 assignment by Mashrur Arafin Ayon
+## This script is written to help solve time-consuming calculation in short time using cachedata
+## The function is divided into two basic segments
 
-## Write a short comment describing this function
+## In the first segment I ensure the process to use cache data
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  k <- NULL
+  set <- function(y){
+    x <<- y
+    k <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) k <<- inverse
+  getInverse <- function() k 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## In the second segment I am using the cached data.
+## It will reduce the calculation time. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
